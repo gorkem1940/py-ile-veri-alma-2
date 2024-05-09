c6f33e1b8766f5b974bd19cb46f8ec44f041e029
gorkem <113816295+gorkem1940@users.noreply.github.com>
2024-05-09 09:43:09 +0300

    veri2.py

--- veri2.py
+++ veri2.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 
 def fetch_and_save_xml():
 
-    url = 'http://192.168.2.162/status.xml'
+    url = 'http://192.168.2.162/status.xml' // verinin alınacağı web sitesi 
 
     headers = {'Authorization': 'Bearer YOUR_ACCESS_TOKEN'}
 
@@ -17,7 +17,8 @@ def fetch_and_save_xml():
         xml_data = response.text
         
 
-        with open('status.xml', 'w') as f:
+        with open('status.xml', 'w') as f: 
+            //xml dosyasının yazılacağı yer status yerine onu yazalım 
             f.write(xml_data)
         print('XML verisi başarıyla dosyaya yazıldı.')
     else:
@@ -27,7 +28,7 @@ def run_schedule():
     schedule.every(1).seconds.do(fetch_and_save_xml)
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(1) // bekleme süresi bir saniyede bir veri yenileyecektir
 
 thread = threading.Thread(target=run_schedule)
 thread.start()
